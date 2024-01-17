78c38739fb8dfdc8b8ca42292eeba1c07ba91628
fluorescent-turtle <sara.grecu25@gmail.com>
2024-01-17 17:05:25 +0100

    Initial commit

---
+++ data_classes.py
@@ -0,0 +1,24 @@
+import json
+from dataclasses import dataclass
+
+
+@dataclass
+class Map:
+    name_field: str
+    length_field: int
+    width_field: int
+    num_blocked_areas: int
+    min_area_blocked: int
+    max_area_blocked: int
+    num_circle_blocked: int
+
+
+@dataclass
+class Robot:
+    type: str
+    speed: int
+    cutting_diameter: int
+    autonomy: int
+    cutting_mode: str
+    bounce_mode: str
+
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import json
+
+
 # The function collects all the data from the user
 def get_data():
     pass
 
 
 # The function produce a json file from the data coming from get_data()
-def produce_json():
+def produce_json(
+
+):
     pass
 
+
+def pippo(name: str):
+    pass
 
 if __name__ == '__main__':
     get_data()
