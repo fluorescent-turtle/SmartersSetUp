10a14c8ae3ba53778ac62741fe2365e55e416f25
fluorescent-turtle <sara.grecu25@gmail.com>
2024-01-22 10:02:05 +0100

    Initial commit

--- data_classes.py
+++ data_classes.py
@@ -2,20 +2,23 @@ from pydantic import BaseModel
 
 
 class Environment(BaseModel):
-    name_field: str = 'Null name'
-    num_blocked_areas: int = 0
-    num_circle_blocked: int = 0
-    length_field: float = 0.0
-    width_field: float = 0.0
-    min_area_blocked: float = 0.0
-    max_area_blocked: float = 0.0
+    num_blocked_areas: int
+    num_blocked_circles: int
+    num_blocked_squares: int
+    length_field: float
+    width_field: float
+    min_area_blocked: float
+    max_area_blocked: float
+    isolated_area_length: float
+    isolated_area_width: float
+    isolated_area_shape: str
 
 
 class Robot(BaseModel):
-    type: str = 'Null type'
-    cutting_mode: str = 'Null cutting mode'
-    bounce_mode: str = 'Null bounce mode'
-    speed: float = 0.0
-    cutting_diameter: float = 0.0
-    autonomy: int = 0
-    environment_name: str = 'Null environment name'
+    type: str
+    cutting_mode: str
+    bounce_mode: str
+    speed: float
+    cutting_diameter: float
+    autonomy: int
+    #environment_name: str
--- gui.py
+++ gui.py
@@ -1,19 +1,20 @@
 import json
 import tkinter as tk
-from tkinter import Tk, ttk
+from tkinter import Tk, ttk, StringVar
 from tkinter.ttk import Button
 
-
-# todo: ogni valore nelle gui deve essere memorizzato nel json
+from data_classes import Robot, Environment
 
 
 # The function produce a json file from the data coming from get_data()
-def produce_json(environment, robot):
-    with open("data_file", "w") as data_file:
-        json.dump(robot.model_dump(), data_file, indent=2)
+def produce_environment_json(environment):
+    with open("environment_file", "w") as environment_file:
+        json.dump(environment.model_dump(), environment_file, indent=2)
 
-def click_done():
-    exit()
+
+def produce_robot_json(robot):
+    with open("robot_file", "w") as robot_file:
+        json.dump(robot.model_dump(), robot_file, indent=2)
 
 
 class RobotWindow(Tk):
@@ -53,9 +54,9 @@ class RobotWindow(Tk):
         first_label.grid(column=0, row=0, sticky='W', **options)
 
         OptionList = ["450X"]
-        robot_type = tk.StringVar()
-        robot_type.set(OptionList[0])
-        robot_entry = tk.OptionMenu(frame, robot_type, *OptionList)
+        self.robot_type = tk.StringVar()
+        self.robot_type.set(OptionList[0])
+        robot_entry = tk.OptionMenu(frame, self.robot_type, *OptionList)
         robot_entry.config(width=20, font=("Helvetica", 12))
         robot_entry.grid(column=1, row=0, **options)
         robot_entry.focus()
@@ -65,9 +66,9 @@ class RobotWindow(Tk):
         second_label.grid(column=0, row=1, sticky='W', **options)
 
         OptionList = ["random", "systematic"]
-        cutting_mode = tk.StringVar()
-        cutting_mode.set(OptionList[0])
-        cutting_mode_entry = tk.OptionMenu(frame, cutting_mode, *OptionList)
+        self.cutting_mode = tk.StringVar()
+        self.cutting_mode.set(OptionList[0])
+        cutting_mode_entry = tk.OptionMenu(frame, self.cutting_mode, *OptionList)
         cutting_mode_entry.config(width=20, font=("Helvetica", 12))
         cutting_mode_entry.grid(column=1, row=1, **options)
         cutting_mode_entry.focus()
@@ -77,19 +78,19 @@ class RobotWindow(Tk):
         third_label.grid(column=0, row=2, sticky='W', **options)
 
         OptionList = ["ping-pong", "probability distribution", "random"]
-        bounce_mode = tk.StringVar()
-        bounce_mode.set(OptionList[0])
-        bounce_mode_entry = tk.OptionMenu(frame, bounce_mode, *OptionList)
+        self.bounce_mode = tk.StringVar()
+        self.bounce_mode.set(OptionList[0])
+        bounce_mode_entry = tk.OptionMenu(frame, self.bounce_mode, *OptionList)
         bounce_mode_entry.config(width=20, font=("Helvetica", 12))
         bounce_mode_entry.grid(column=1, row=2, **options)
         bounce_mode_entry.focus()
 
         # speed
-        fourth_label = ttk.Label(frame, text='Speed: ')
+        fourth_label = ttk.Label(frame, text='Speed (km/h): ')
         fourth_label.grid(column=0, row=3, sticky='W', **options)
 
-        speed = tk.StringVar()
-        speed_entry = ttk.Entry(frame, textvariable=speed)
+        self.speed = tk.StringVar()
+        speed_entry = ttk.Entry(frame, textvariable=self.speed)
         speed_entry.grid(column=1, row=3, **options)
         speed_entry.focus()
 
@@ -97,17 +98,17 @@ class RobotWindow(Tk):
         fifth_label = ttk.Label(frame, text='Cutting diameter: ')
         fifth_label.grid(column=0, row=4, sticky='W', **options)
 
-        cutting_diameter = tk.StringVar()
-        cutting_diameter_entry = ttk.Entry(frame, textvariable=cutting_diameter)
+        self.cutting_diameter = tk.StringVar()
+        cutting_diameter_entry = ttk.Entry(frame, textvariable=self.cutting_diameter)
         cutting_diameter_entry.grid(column=1, row=4, **options)
         cutting_diameter_entry.focus()
 
         # autonomy
-        sixth_label = ttk.Label(frame, text='Autonomy: ')
+        sixth_label = ttk.Label(frame, text='Autonomy (hours): ')
         sixth_label.grid(column=0, row=5, sticky='W', **options)
 
-        autonomy = tk.StringVar()
-        autonomy_entry = ttk.Entry(frame, textvariable=autonomy)
+        self.autonomy: StringVar = tk.StringVar()
+        autonomy_entry = ttk.Entry(frame, textvariable=self.autonomy)
         autonomy_entry.grid(column=1, row=5, **options)
         autonomy_entry.focus()
 
@@ -118,7 +119,10 @@ class RobotWindow(Tk):
         # add padding to the frame and show it
         frame.grid(padx=20, pady=20)
 
+
     def click_next(self):
+        produce_robot_json(Robot(type=self.robot_type.get(), cutting_mode=self.cutting_mode.get(), bounce_mode=self.bounce_mode.get(), speed=float(self.speed.get()), cutting_diameter=float(self.cutting_diameter.get()), autonomy=float(self.autonomy.get())))
+
         for widget in self.winfo_children():
             widget.destroy()
         self.destroy()
@@ -160,8 +164,8 @@ class EnvironmentWindow(Tk):
         first_label = ttk.Label(frame, text='Length (environment): ')
         first_label.grid(column=0, row=0, sticky='W', **options)
 
-        length = tk.StringVar()
-        length_entry = ttk.Entry(frame, textvariable=length)
+        self.length = tk.IntVar()
+        length_entry = ttk.Entry(frame, textvariable=self.length)
         length_entry.grid(column=1, row=0, **options)
         length_entry.focus()
 
@@ -169,8 +173,8 @@ class EnvironmentWindow(Tk):
         second_label = ttk.Label(frame, text='Width (environment): ')
         second_label.grid(column=0, row=1, sticky='W', **options)
 
-        width = tk.StringVar()
-        width_entry = ttk.Entry(frame, textvariable=width)
+        self.width = tk.IntVar()
+        width_entry = ttk.Entry(frame, textvariable=self.width)
         width_entry.grid(column=1, row=1, **options)
         width_entry.focus()
 
@@ -178,8 +182,8 @@ class EnvironmentWindow(Tk):
         third_label = ttk.Label(frame, text='Blocked areas (number): ')
         third_label.grid(column=0, row=2, sticky='W', **options)
 
-        num_blocked_areas = tk.StringVar()
-        num_blocked_areas_entry = ttk.Entry(frame, textvariable=num_blocked_areas)
+        self.num_blocked_areas = tk.IntVar()
+        num_blocked_areas_entry = ttk.Entry(frame, textvariable=self.num_blocked_areas)
         num_blocked_areas_entry.grid(column=1, row=2, **options)
         num_blocked_areas_entry.focus()
 
@@ -187,8 +191,8 @@ class EnvironmentWindow(Tk):
         fourth_label = ttk.Label(frame, text='Min area blocked areas: ')
         fourth_label.grid(column=0, row=3, sticky='W', **options)
 
-        min_area = tk.StringVar()
-        min_area_entry = ttk.Entry(frame, textvariable=min_area)
+        self.min_area = tk.StringVar()
+        min_area_entry = ttk.Entry(frame, textvariable=self.min_area)
         min_area_entry.grid(column=1, row=3, **options)
         min_area_entry.focus()
 
@@ -196,8 +200,8 @@ class EnvironmentWindow(Tk):
         fifth_label = ttk.Label(frame, text='Max area blocked areas: ')
         fifth_label.grid(column=0, row=4, sticky='W', **options)
 
-        max_area = tk.StringVar()
-        max_area_entry = ttk.Entry(frame, textvariable=max_area)
+        self.max_area = tk.StringVar()
+        max_area_entry = ttk.Entry(frame, textvariable=self.max_area)
         max_area_entry.grid(column=1, row=4, **options)
         max_area_entry.focus()
 
@@ -205,8 +209,8 @@ class EnvironmentWindow(Tk):
         sixth_label = ttk.Label(frame, text='Number of circles (blocked areas): ')
         sixth_label.grid(column=0, row=5, sticky='W', **options)
 
-        circles = tk.StringVar()
-        circles_entry = ttk.Entry(frame, textvariable=circles)
+        self.circles = tk.StringVar()
+        circles_entry = ttk.Entry(frame, textvariable=self.circles)
         circles_entry.grid(column=1, row=5, **options)
         circles_entry.focus()
 
@@ -214,8 +218,8 @@ class EnvironmentWindow(Tk):
         seventh_label = ttk.Label(frame, text='Number of squares (blocked areas): ')
         seventh_label.grid(column=0, row=6, sticky='W', **options)
 
-        squares = tk.StringVar()
-        squares_entry = ttk.Entry(frame, textvariable=squares)
+        self.squares = tk.StringVar()
+        squares_entry = ttk.Entry(frame, textvariable=self.squares)
         squares_entry.grid(column=1, row=6, **options)
         squares_entry.focus()
 
@@ -223,16 +227,16 @@ class EnvironmentWindow(Tk):
         eighth_label = ttk.Label(frame, text='Length (isolated area): ')
         eighth_label.grid(column=0, row=8, sticky='W', **options)
 
-        isolated_area_len = tk.StringVar()
-        isolated_area_len_entry = ttk.Entry(frame, textvariable=isolated_area_len)
+        self.isolated_area_len = tk.StringVar()
+        isolated_area_len_entry = ttk.Entry(frame, textvariable=self.isolated_area_len)
         isolated_area_len_entry.grid(column=1, row=8, **options)
         isolated_area_len_entry.focus()
 
         ninth_label = ttk.Label(frame, text='Width (isolated area): ')
         ninth_label.grid(column=0, row=9, sticky='W', **options)
 
-        isolated_area_wid = tk.StringVar()
-        isolated_area_wid_entry = ttk.Entry(frame, textvariable=isolated_area_wid)
+        self.isolated_area_wid = tk.StringVar()
+        isolated_area_wid_entry = ttk.Entry(frame, textvariable=self.isolated_area_wid)
         isolated_area_wid_entry.grid(column=1, row=9, **options)
         isolated_area_wid_entry.focus()
 
@@ -240,16 +244,19 @@ class EnvironmentWindow(Tk):
         tenth_label.grid(column=0, row=10, sticky='W', **options)
 
         OptionList = ["Square", "Circle"]
-        shape = tk.StringVar()
-        shape.set(OptionList[0])
-        shape_entry = tk.OptionMenu(frame, shape, *OptionList)
+        self.shape = tk.StringVar()
+        self.shape.set(OptionList[0])
+        shape_entry = tk.OptionMenu(frame, self.shape, *OptionList)
         shape_entry.config(width=20, font=("Helvetica", 12))
         shape_entry.grid(column=1, row=10, **options)
         shape_entry.focus()
 
         # Done button
-        done_button = Button(self, text="Done", command=click_done)
+        done_button = Button(self, text="Done", command=self.click_done)
         done_button.place(x=800, y=820)
 
         # add padding to the frame and show it
         frame.grid(padx=20, pady=20)
+    def click_done(self):
+        produce_environment_json(Environment(num_blocked_areas=self.num_blocked_areas.get(), num_blocked_circles=self.circles.get(), num_blocked_squares=self.squares.get(), length_field=float(self.length.get()), width_field=float(self.width.get()), min_area_blocked=float(self.min_area.get()), max_area_blocked=float(self.max_area.get()), isolated_area_length=float(self.isolated_area_len.get()), isolated_area_width=float(self.isolated_area_wid.get()), isolated_area_shape=self.shape.get()))
+        exit()
