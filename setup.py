e77bce2b77e6b188f24fbf525779d8cf22509c92
Sara Grecu <sara.grecu25@gmail.com>
2024-09-16 14:26:13 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    install_requires=[
-    ],
-    packages=["SetUp"],
-    include_package_data=True,
-    entry_points={"console_scripts": ["realpython=SetUp.main:main"]},
-)
